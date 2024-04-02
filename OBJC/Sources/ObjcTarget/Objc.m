7094404e2cbe40cfd5a4f9c5319ea20a778a9129
Daniel Bachar <danielbac@zillowgroup.com>
2024-04-02 13:13:55 -0600

    updates

--- OBJC/Sources/ObjcTarget/Objc.m
+++ OBJC/Sources/ObjcTarget/Objc.m
@@ -24,4 +24,19 @@
  __END_DECLS
 
  */
-// resource_bundle_accessor.m
+
+// resource_bundle_accessor.m - defined here - https://github.com/apple/swift-package-manager/blob/26b4ed010e7ec211f0aad7527961f83508ea4771/Sources/Build/BuildDescription/ClangTargetBuildDescription.swift#L439
+/*
+#import <Foundation/Foundation.h>
+
+NSBundle* Package_ObjcTarget_SWIFTPM_MODULE_BUNDLE() {
+    NSURL *bundleURL = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"\(bundleBasename)"];
+
+    NSBundle *preferredBundle = [NSBundle bundleWithURL:bundleURL];
+    if (preferredBundle == nil) {
+        return [NSBundle bundleWithPath:@"\(bundlePath.pathString)"];
+    }
+
+    return preferredBundle;
+}
+*/
